7ddf7e3ae24381cc8bbb892caaeb7dd8b8cae165
adarsha <adarsh@gmail.com>
2023-08-16 18:09:06 +0545

    note of query

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 app.get("/:name/:lastName", (req, res) => {
   const { name, lastName } = req.params;
-  res.send(`your name is ${name} ${lastName}`);
+  const { address, phone } = req.query;
+  res.send(`your name is ${name} ${lastName} my address is ${address} and ${phone}`);
 });
-app.listen(3000);
+app.listen(3001);
 console.log("your code is running in port 3000");
