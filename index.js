c2a978734128afcb7e8c8c513f8a17b9ae701e24
adarsha <adarsh@gmail.com>
2023-08-17 17:23:47 +0545

    routing concept to minimize the code debugging

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const app = express();
-app.get("/:name/:lastName", (req, res) => {
-  const { name, lastName } = req.params;
-  const { address, phone } = req.query;
-  res.send(`your name is ${name} ${lastName} my address is ${address} and ${phone}`);
-});
-app.listen(3001);
-console.log("your code is running in port 3000");
+const indexRouter= require("./routes");
+const app=express();
+
+app.use("/",indexRouter);
+
+app.listen(3003);
+console.log("your code is running in port 3002");
